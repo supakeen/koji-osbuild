cd056591ef627bebd44d5d6cb369e2e465876a82
Simon de Vlieger <supakeen@redhat.com>
2025-02-19 22:47:49 +0100

    wip! cli plugin
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

---
+++ plugins/cli/imagebuilder.py
@@ -0,0 +1,235 @@
+import json
+import optparse  # pylint: disable=deprecated-module
+from pprint import pprint
+
+import koji
+import koji_cli.lib as kl
+from koji.plugin import export_cli
+
+
+def parse_repo(_option, _opt, value, parser):
+    repo = parser.values.repo
+    if repo and isinstance(repo[0], dict):
+        repo.append({"baseurl": value})
+        return
+
+    if not repo:
+        parser.values.repo = repo = []
+    repo.append(value)
+
+
+def parse_repo_package_set(_option, opt, value, parser):
+    if not parser.values.repo:
+        raise optparse.OptionValueError(f"Need '--repo' for {opt}")
+
+    repo = parser.values.repo.pop()
+    if not isinstance(repo, dict):
+        repo = {"baseurl": repo}
+    ps = repo.get("package_sets", [])
+    vals = set(map(lambda x: x.strip(), value.split(";")))
+    repo["package_sets"] = list(sorted(set(ps).union(vals)))
+    parser.values.repo.append(repo)
+
+
+def parse_args(argv):
+    usage = (
+        "usage: %prog image-builder-build [options] <name> <version> "
+        "<distro> <target> <arch> [<arch> ...]"
+    )
+
+    parser = kl.OptionParser(usage=kl.get_usage_str(usage))
+
+    parser.add_option(
+        "--customizations",
+        type=str,
+        default=None,
+        dest="customizations",
+        help="Additional customizations to pass to Composer (json file)",
+    )
+    parser.add_option(
+        "--upload-options",
+        type=str,
+        default=None,
+        dest="upload_options",
+        help="Cloud target upload options (json file)",
+    )
+    parser.add_option(
+        "--nowait",
+        action="store_false",
+        dest="wait",
+        help="Don't wait on image creation",
+    )
+    parser.add_option(
+        "--ostree-parent",
+        type=str,
+        dest="ostree_parent",
+        help="The OSTree commit parent for OSTree commit image types",
+    )
+    parser.add_option(
+        "--ostree-ref",
+        type=str,
+        dest="ostree_ref",
+        help="The OSTree commit ref for OSTree commit image types",
+    )
+    parser.add_option(
+        "--ostree-url",
+        type=str,
+        dest="ostree_url",
+        help="URL to the OSTree repo for OSTree commit image types",
+    )
+    parser.add_option("--release", help="Forcibly set the release field")
+    parser.add_option(
+        "--repo",
+        action="callback",
+        callback=parse_repo,
+        nargs=1,
+        type=str,
+        help=(
+            "Specify a repo that will override the repo used to install "
+            "RPMs in the image. May be used multiple times. The "
+            "build tag repo associated with the target is the default."
+        ),
+    )
+    parser.add_option(
+        "--repo-package-sets",
+        dest="repo",
+        nargs=1,
+        type=str,
+        action="callback",
+        callback=parse_repo_package_set,
+        help=(
+            "Specify the package sets for the last repository. "
+            "Individual set items are separated by ';'. "
+            "Maybe be used multiple times"
+        ),
+    )
+    parser.add_option(
+        "--image-type",
+        metavar="TYPE",
+        help="Request an image-type [default: guest-image]",
+        type=str,
+        default="guest-image",
+    )
+    parser.add_option(
+        "--skip-tag", action="store_true", help="Do not attempt to tag package"
+    )
+    parser.add_option(
+        "--wait",
+        action="store_true",
+        help="Wait on the image creation, even if running in the background",
+    )
+
+    opts, args = parser.parse_args(argv)
+    if len(args) < 5:
+        parser.error(
+            "At least five arguments are required: a name, "
+            "a version, a distribution, a build target, "
+            "and 1 or more architectures."
+        )
+
+    for i, arg in enumerate(("name", "version", "distro", "target")):
+        setattr(opts, arg, args[i])
+    setattr(opts, "arch", args[4:])
+
+    return opts
+
+
+def check_target(session, name):
+    """Check the target with name exists and has a destination tag"""
+
+    target = session.getBuildTarget(name)
+    if not target:
+        raise koji.GenericError(f"Unknown build target: {name}")
+
+    tag = session.getTag(target["dest_tag"])
+    if not tag:
+        raise koji.GenericError(
+            f"Unknown destination tag: {target['dest_tag_name']}"
+        )
+
+
+# pylint: disable=too-many-branches
+@export_cli
+def handle_imagebuilder_build(options, session, argv):
+    "[build] Build images via image-builder"
+    args = parse_args(argv)
+
+    name, version, arch, target = (
+        args.name,
+        args.version,
+        args.arch,
+        args.target,
+    )
+    distro, image_type = args.distro, args.image_type
+
+    opts = {}
+
+    if args.release:
+        opts["release"] = args.release
+
+    if args.repo:
+        opts["repo"] = args.repo
+
+    if args.skip_tag:
+        opts["skip_tag"] = True
+
+    # ostree command line parameters
+    ostree = {}
+
+    if args.ostree_parent:
+        ostree["parent"] = args.ostree_parent
+
+    if args.ostree_ref:
+        ostree["ref"] = args.ostree_ref
+
+    if args.ostree_url:
+        ostree["url"] = args.ostree_url
+
+    if ostree:
+        opts["ostree"] = ostree
+
+    # customizations handling
+    if args.customizations:
+        with open(args.customizations, "r", encoding="utf-8") as f:
+            opts["customizations"] = json.load(f)
+
+    # cloud upload options handling
+    if args.upload_options:
+        with open(args.upload_options, "r", encoding="utf-8") as f:
+            opts["upload_options"] = json.load(f)
+
+    # Do some early checks to be able to give quick feedback
+    check_target(session, target)
+
+    if not options.quiet:
+        print("name:", name)
+        print("version:", version)
+        print("distro:", distro)
+        print("arches:", ", ".join(arch))
+        print("target:", target)
+        print("image type:", image_type)
+        pprint(opts)
+
+    kl.activate_session(session, options)
+
+    task_id = session.imageBuilderBuild(
+        name, version, distro, image_type, target, arch, opts=opts
+    )
+
+    if not options.quiet:
+        print(f"Created task: {task_id}")
+        print(f"Task info: {options.weburl}/taskinfo?taskID={task_id}")
+
+    # pylint: disable=protected-access
+    if (args.wait is None and kl._running_in_bg()) or args.wait is False:
+        # either running in the background or must not wait by user's
+        # request. All done.
+        return None
+
+    session.logout()
+    res = kl.watch_tasks(session, [task_id], quiet=options.quiet)
+
+    if res == 0:
+        result = session.getTaskResult(task_id)
+        print(result)
+    return res
---
+++ test/unit/test_cli_imagebuilder.py
@@ -0,0 +1,368 @@
+#
+# koji command line interface plugin unit tests
+#
+
+
+import contextlib
+import io
+import json
+import os
+import tempfile
+
+import koji
+import koji_cli.lib as kl
+from flexmock import flexmock
+
+from plugintest import ImageBuilderPluginTest
+
+
+@ImageBuilderPluginTest.load_plugin("cli")
+class TestCliPlugin(ImageBuilderPluginTest):
+
+    @staticmethod
+    def mock_koji_lib(*, bg=False, task_result=0):
+        kojilib = flexmock(OptionParser=kl.OptionParser)
+
+        kojilib.should_receive("get_usage_str").and_return("usage")
+        kojilib.should_receive("activate_session").once()
+        kojilib.should_receive("_running_in_bg").and_return(bg)
+        kojilib.should_receive("watch_tasks").and_return(task_result)
+
+        return kojilib
+
+    @staticmethod
+    def mock_options(*, quiet=False):
+        options = flexmock(quiet=quiet, weburl="http://osbuild.org/")
+        return options
+
+    @staticmethod
+    def mock_session_add_valid_tag(session):
+        build_target = {
+            "build_tag": 23,
+            "build_tag_name": "target-build",
+            "dest_tag": 42,
+            "dest_tag_name": "target-dest",
+        }
+
+        tag_info = {
+            "id": build_target["dest_tag"],
+            "name": build_target["dest_tag_name"],
+            "locked": False,
+        }
+
+        session.should_receive("getBuildTarget").with_args("target").and_return(
+            build_target
+        ).once()
+
+        session.should_receive("getTag").with_args(
+            build_target["dest_tag"]
+        ).and_return(tag_info).once()
+
+        return session
+
+    def test_basic_invocation(self):
+        # check we get the right amount of arguments
+        # i.e. we are missing the architecture here
+        argv = ["name", "version", "distro", "target"]
+
+        f = io.StringIO()
+        with self.assertRaises(SystemExit) as cm, contextlib.redirect_stderr(f):
+            self.plugin.handle_imagebuilder_build(None, None, argv)
+            self.assertEqual(cm.exception.code, 2)
+            self.assertIn("osbuild-image", f.getvalue())
+        f.close()
+
+        # check one successful invocation
+
+        argv = [
+            # the required positional arguments
+            "name",
+            "version",
+            "distro",
+            "target",
+            "arch1",
+            # optional keyword arguments
+            "--repo",
+            "https://first.repo",
+            "--repo",
+            "https://second.repo",
+            "--release",
+            "20200202.n2",
+            "--skip-tag",
+        ]
+
+        expected_args = [
+            "name",
+            "version",
+            "distro",
+            "guest-image",  # the default image type
+            "target",
+            ["arch1"],
+        ]
+
+        expected_opts = {
+            "release": "20200202.n2",
+            "repo": ["https://first.repo", "https://second.repo"],
+            "skip_tag": True,
+        }
+
+        task_result = {"compose_id": "42", "build_id": 23}
+        task_id = 1
+        koji_lib = self.mock_koji_lib()
+
+        options = self.mock_options()
+        session = flexmock()
+
+        self.mock_session_add_valid_tag(session)
+
+        session.should_receive("imageBuilderBuild").with_args(
+            *expected_args, opts=expected_opts
+        ).and_return(task_id).once()
+
+        session.should_receive("logout").with_args().once()
+
+        session.should_receive("getTaskResult").with_args(task_id).and_return(
+            task_result
+        ).once()
+
+        setattr(self.plugin, "kl", koji_lib)
+        r = self.plugin.handle_imagebuilder_build(options, session, argv)
+        self.assertEqual(r, 0)
+
+    def test_customizations_options(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+
+            customizations = {"packages": ["emacs"]}
+
+            path = os.path.join(tmpdir, "customizations.json")
+
+            with open(path, "w", encoding="utf-8") as f:
+                json.dump(customizations, f)
+
+            argv = [
+                # the required positional arguments
+                "name",
+                "version",
+                "distro",
+                "target",
+                "arch1",
+                # optional keyword arguments
+                "--repo",
+                "https://first.repo",
+                "--repo",
+                "https://second.repo",
+                "--release",
+                "20200202.n2",
+                "--customizations",
+                path,
+            ]
+
+            expected_args = [
+                "name",
+                "version",
+                "distro",
+                "guest-image",  # the default image type
+                "target",
+                ["arch1"],
+            ]
+
+            expected_opts = {
+                "release": "20200202.n2",
+                "repo": ["https://first.repo", "https://second.repo"],
+                "customizations": customizations,
+            }
+
+            task_result = {"compose_id": "42", "build_id": 23}
+            task_id = 1
+            koji_lib = self.mock_koji_lib()
+
+            options = self.mock_options()
+            session = flexmock()
+
+            self.mock_session_add_valid_tag(session)
+
+            session.should_receive("imageBuilderBuild").with_args(
+                *expected_args, opts=expected_opts
+            ).and_return(task_id).once()
+
+            session.should_receive("logout").with_args().once()
+
+            session.should_receive("getTaskResult").with_args(
+                task_id
+            ).and_return(task_result).once()
+
+            setattr(self.plugin, "kl", koji_lib)
+            r = self.plugin.handle_imagebuilder_build(options, session, argv)
+            self.assertEqual(r, 0)
+
+    def test_ostree_options(self):
+        # Check we properly handle ostree specific options
+
+        argv = [
+            # the required positional arguments
+            "name",
+            "version",
+            "distro",
+            "target",
+            "arch1",
+            # optional keyword arguments
+            "--repo",
+            "https://first.repo",
+            "--repo",
+            "https://second.repo",
+            "--release",
+            "20200202.n2",
+            "--ostree-parent",
+            "ostree/$arch/staging",
+            "--ostree-ref",
+            "ostree/$arch/production",
+            "--ostree-url",
+            "https://osbuild.org/repo",
+        ]
+
+        expected_args = [
+            "name",
+            "version",
+            "distro",
+            "guest-image",  # the default image type
+            "target",
+            ["arch1"],
+        ]
+
+        expected_opts = {
+            "release": "20200202.n2",
+            "repo": ["https://first.repo", "https://second.repo"],
+            "ostree": {
+                "parent": "ostree/$arch/staging",
+                "ref": "ostree/$arch/production",
+                "url": "https://osbuild.org/repo",
+            },
+        }
+
+        task_result = {"compose_id": "42", "build_id": 23}
+        task_id = 1
+        koji_lib = self.mock_koji_lib()
+
+        options = self.mock_options()
+        session = flexmock()
+
+        self.mock_session_add_valid_tag(session)
+
+        session.should_receive("imageBuilderBuild").with_args(
+            *expected_args, opts=expected_opts
+        ).and_return(task_id).once()
+
+        session.should_receive("logout").with_args().once()
+
+        session.should_receive("getTaskResult").with_args(task_id).and_return(
+            task_result
+        ).once()
+
+        setattr(self.plugin, "kl", koji_lib)
+        r = self.plugin.handle_imagebuilder_build(options, session, argv)
+        self.assertEqual(r, 0)
+
+    def test_repo_package_sets(self):
+        # Check we properly handle ostree specific options
+
+        argv = [
+            # the required positional arguments
+            "name",
+            "version",
+            "distro",
+            "target",
+            "arch1",
+            # optional keyword arguments
+            "--repo",
+            "https://first.repo",
+            "--repo-package-sets",
+            "a; b; c",
+            "--repo-package-sets",
+            "d",
+            "--repo",
+            "https://second.repo",
+            "--repo-package-sets",
+            "alpha",
+            "--repo",
+            "https://third.repo",  # NB: no `--repo-package-set`
+            "--release",
+            "20200202.n2",
+        ]
+
+        expected_args = [
+            "name",
+            "version",
+            "distro",
+            "guest-image",  # the default image type
+            "target",
+            ["arch1"],
+        ]
+
+        expected_opts = {
+            "release": "20200202.n2",
+            "repo": [
+                {
+                    "baseurl": "https://first.repo",
+                    "package_sets": ["a", "b", "c", "d"],
+                },
+                {"baseurl": "https://second.repo", "package_sets": ["alpha"]},
+                {"baseurl": "https://third.repo"},
+            ],
+        }
+
+        task_result = {"compose_id": "42", "build_id": 23}
+        task_id = 1
+        koji_lib = self.mock_koji_lib()
+
+        options = self.mock_options()
+        session = flexmock()
+
+        self.mock_session_add_valid_tag(session)
+
+        session.should_receive("imageBuilderBuild").with_args(
+            *expected_args, opts=expected_opts
+        ).and_return(task_id).once()
+
+        session.should_receive("logout").with_args().once()
+
+        session.should_receive("getTaskResult").with_args(task_id).and_return(
+            task_result
+        ).once()
+
+        setattr(self.plugin, "kl", koji_lib)
+        r = self.plugin.handle_imagebuilder_build(options, session, argv)
+        self.assertEqual(r, 0)
+
+    def test_target_check(self):
+        # unknown build target
+        session = flexmock()
+
+        session.should_receive("getBuildTarget").with_args("target").and_return(
+            None
+        ).once()
+
+        argv = ["name", "version", "distro", "target", "arch1"]
+        with self.assertRaises(koji.GenericError):
+            self.plugin.handle_imagebuilder_build(None, session, argv)
+
+        # unknown destination tag
+        build_target = {
+            "build_tag": 23,
+            "build_tag_name": "target-build",
+            "dest_tag": 42,
+            "dest_tag_name": "target-dest",  # missing!
+        }
+
+        session = flexmock()
+
+        session.should_receive("getBuildTarget").with_args("target").and_return(
+            build_target
+        ).once()
+
+        session.should_receive("getTag").with_args(
+            build_target["dest_tag"]
+        ).and_return(None).once()
+
+        argv = ["name", "version", "distro", "target", "arch1"]
+        with self.assertRaises(koji.GenericError):
+            self.plugin.handle_imagebuilder_build(None, session, argv)
